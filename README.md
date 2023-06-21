e7bc31ffc397592c52094d35ceb7c16c0eac6af5
Dan Phung <leftbyte@gmail.com>
2023-06-21 16:30:43 -0700

    implemented hillbilly sayings

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # hillbilly-chat
-What would a hillbilly say?
+Example chatbot that takes a phrase and translates it to what a quirky hillbilly would say.
 
+# Usage
+
+Create an OpenAI API key and export the key.
+
+E.g. export OPENAI_API_KEY=sk-abc123.
+
+Install the requirements.txt and run 'python billy.py'. Open the given link
+in a browser.
+
+E.g. http://127.0.0.1:7860
--- billy.py
+++ billy.py
@@ -1,4 +1,54 @@
-import openai
-import gradio as gr
+#!/usr/bin/env python
 
-openai.api_key = "OPENAI_API_KEY"
+import openai
+import os
+import gradio as gr
+import typer
+
+"""
+Example of a one-turn conversation with a hillbilly chatbot.
+"""
+
+VERSION = "1.0.0"
+
+
+def billy(input: str = None):
+    """Hillbily chatbot that tranlates phrases to funny old sayings."""
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a quirky hillbilly from the 1920s that speaks in colloquialisms.",
+        },
+    ]
+
+    CONTEXT = "What is a hillbillly colloquialism for: "
+    if input:
+        messages.append({"role": "user", "content": CONTEXT + input})
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+
+        reply = chat.choices[0].message.content
+        messages.append({"role": "assistant", "content": reply})
+        return reply
+
+
+def main():
+    apikey = os.getenv("OPENAI_API_KEY")
+    openai.api_key = apikey
+
+    inputs = gr.Textbox(lines=7, label="Enter in a phrase to translate:")
+    outputs = gr.Textbox(label="I reckon I'd say:")
+
+    gr.Interface(
+        fn=billy,
+        inputs=inputs,
+        outputs=outputs,
+        title=f"Colloquial Chatterbot {VERSION}",
+        description="Things a hillbilly would say",
+        theme=gr.themes.Monochrome(),
+    ).launch(share=True)
+
+
+if __name__ == "__main__":
+    typer.run(main)
